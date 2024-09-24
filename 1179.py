763e4993cd02373a4415bfe9c1ea7734e45fcfdb
DaviJ13 <davij130603@gmail.com>
2024-09-24 14:50:44 -0300

    Create 1179.py

---
+++ 1179.py
@@ -0,0 +1,24 @@
+par = []
+impar = []
+
+for i in range(15):
+    x = int(input())
+    
+    if x % 2 == 0:
+        par.append(x)
+        if len(par) == 5:
+            for j in range(5):
+                print(f"par[{j}] = {par[j]}")
+            par = []
+    else:
+        impar.append(x)
+        if len(impar) == 5:
+            for j in range(5):
+                print(f"impar[{j}] = {impar[j]}")
+            impar = []
+
+for j in range(len(impar)):
+    print(f"impar[{j}] = {impar[j]}")
+
+for j in range(len(par)):
+    print(f"par[{j}] = {par[j]}")
