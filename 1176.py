64c3db424f3cfd78d1c9b0ec3c799c3fef9cca12
Jose Davi <davij130603@gmail.com>
2024-09-24 14:54:26 -0300

    Create 1176.py

---
+++ 1176.py
@@ -0,0 +1,17 @@
+def fib(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    a, b = 0, 1
+
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+a = int(input())
+
+for i in range(a):
+    x = int(input())
+    print(f"Fib({x}) = {fib(x)}")
